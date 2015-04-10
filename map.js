0881a9c97f9bd53254546a639424eb1b017d7884
Cody McNamara <codyjamesmcnamara@gmail.com>
2015-04-10 12:51:11 -0700

    map shows route from searched address to end address

--- map.js
+++ map.js
@@ -1,56 +1,62 @@
 var service;
 var infowindow
 var map;
+var marker;
+var end = new google.maps.LatLng(37.7856360, -122.3971190)
+var directionsDisplay;
+var directionsService = new google.maps.DirectionsService();
 
 function initialize() {
 
   var sf = new google.maps.LatLng(37.7856360, -122.3971190);
 
+  directionsDisplay = new google.maps.DirectionsRenderer();
+
+
   var mapOptions = {
     center: sf,
-    zoom: 10
+    zoom: 12,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
   };
 
   map = new google.maps.Map(document.getElementById('map-canvas'),
       mapOptions);
 
+  directionsDisplay.setMap(map);
+
+  var input = (document.getElementById('pac-input'));
+
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+  var searchBox = new google.maps.places.SearchBox((input));
+
+  // [START region_getplaces]
+  // Listen for the event fired when the user selects an item from the
+  // pick list. Retrieve the matching places for that item.
+  google.maps.event.addListener(searchBox, 'places_changed', function() {
+    var places = searchBox.getPlaces();
+
+    var start = new google.maps.LatLng(places[0].geometry.location.k, places[0].geometry.location.D)
+
+    calcRoute(start);
+
+  });
+}
+
+
+
+function calcRoute(start) {
   var request = {
-    location: sf,
-    radius: '500',
-    query: 'restaurant'
+      origin: start,
+      destination: end,
+      travelMode: google.maps.TravelMode.DRIVING
   };
-
-  infowindow = new google.maps.InfoWindow();
-  service = new google.maps.places.PlacesService(map);
-  // service.textSearch(request, callback);
-}
-
-
-
-
-
-function callback(results, status) {
-  if (status == google.maps.places.PlacesServiceStatus.OK) {
-    for (var i = 0; i < results.length; i++) {
-      var place = results[i];
-      createMarker(results[i]);
+  directionsService.route(request, function(response, status) {
+    if (status == google.maps.DirectionsStatus.OK) {
+      directionsDisplay.setDirections(response);
     }
-  }
-};
-
-function createMarker(place) {
-  var placeLoc = place.geometry.location;
-  var marker = new google.maps.Marker({
-    map: map,
-    position: place.geometry.location
-  });
-
-  google.maps.event.addListener(marker, 'click', function() {
-    infowindow.setContent(place.name);
-    infowindow.open(map, this);
   });
 }
-
 
 
 google.maps.event.addDomListener(window, 'load', initialize);
---
+++ places.js
@@ -0,0 +1,45 @@
+var markers = [];
+
+
+    if (places.length == 0) {
+      return;
+    }
+    for (var i = 0, marker; marker = markers[i]; i++) {
+      marker.setMap(null);
+    }
+
+    // For each place, get the icon, place name, and location.
+    markers = [];
+    var bounds = new google.maps.LatLngBounds();
+    for (var i = 0, place; place = places[i]; i++) {
+      var image = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25)
+      };
+
+
+
+      // Create a marker for each place.
+      var marker = new google.maps.Marker({
+        map: map,
+        icon: image,
+        title: place.name,
+        position: place.geometry.location
+      });
+
+      markers.push(marker);
+
+      bounds.extend(place.geometry.location);
+    }
+
+    map.fitBounds(bounds);
+
+    // Bias the SearchBox results towards places that are within the bounds of the
+    // current map's viewport.
+    google.maps.event.addListener(map, 'bounds_changed', function() {
+      var bounds = map.getBounds();
+      searchBox.setBounds(bounds);
+    });
